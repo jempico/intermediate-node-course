2f834a5e8f5b73d66aee5d99b842bdd0c5173219
Jemimah <jpfilarca@gmail.com>
2021-03-29 10:19:06 +0200

    refactored

--- server.js
+++ server.js
@@ -14,102 +14,57 @@ app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
 })
 
+
 // CREATE: Go to Postman to see Post request
 app.post('/users',(req,res)=>{
   User.create(
-    {
-      name:req.body.newData.name,
-      email:req.body.newData.email,
-      password:req.body.newData.password
-    },
-    (err,data)=>{
-    if (err){
-      res.json({success: false,message: err})
-    } else if (!data){
-      res.json({success: false,message: "Not Found"})
-    } else {
-      res.json({success: true,data: data})
-    }
+    {...req.body.newData}, 
+    (err,data)=>{ sendResponse(res, err, data)}
+    )
   })
-  })
-
 
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
   User.findById(req.params.id,(err,data)=>{
-    if (err){
-      res.json({
-        success: false,
-        message: err
-      })
-    } else if (!data){
-      res.json({
-        success: false,
-        message: "Not Found"
-      })
-    } else {
-      res.json({
-        success: true,
-        data: data
-      })
-    }
+    sendResponse(res, err, data)
   })
 })
 // UPDATE
 .put((req,res)=>{
   User.findByIdAndUpdate(
     req.params.id,
-    {
-      name:req.body.newData.name,
-      email:req.body.newData.email,
-      password:req.body.newData.password
-    },
+    {...req.body.newData}, //>Using spread operator so as to be able to update each record passing only the body parameter to update. Otherwise, if the values were not included, they would be set to "null". 
     {
       new:true
     },
-    (err,data)=>{
-      if (err){
-        res.json({
-          success: false,
-          message: err
-        })
-      } else if (!data){
-        res.json({
-          success: false,
-          message: "Not Found"
-        })
-      } else {
-        res.json({
-          success: true,
-          data: data
-        })
-      }
-    }
+    (err,data)=>{ sendResponse(res, err, data) }
   )
 })
 // DELETE
 .delete((req,res)=>{
   User.findByIdAndDelete(
     req.params.id,
-    (err,data)=>{
-      if (err){
-        res.json({
-          success: false,
-          message: err
-        })
-      } else if (!data){
-        res.json({
-          success: false,
-          message: "Not Found"
-        })
-      } else {
-        res.json({
-          success: true,
-          data: data
-        })
-      }
-    }
+    (err,data)=>{ sendResponse(res, err, data)}
   )
 })
+
+//Managing Response Callback
+
+function sendResponse(res, err, data){
+  if (err){
+    res.json({
+      success: false,
+      message: err
+    })
+  } else if (!data){
+    res.json({
+      success: false,
+      message: "Not Found"
+    })
+  } else {
+    res.json({success: true,data: data
+    })
+  }
+}
